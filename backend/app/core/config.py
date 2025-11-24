bff098ac4503785ed2b46d0bbdbcb6037fce3b79
Yigtwxx <yigiterdogan023@gmail.com>
2025-11-24 15:52:00 +0300

    New

---
+++ backend/app/core/config.py
@@ -0,0 +1,13 @@
+# backend/app/core/config.py
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "WaterPulse API"
+    API_V1_STR: str = "/api/v1"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
